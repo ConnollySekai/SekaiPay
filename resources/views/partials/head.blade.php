4cbfca2e8c607679f0b2177389a7feaf87e4f6cf
ConnollySekai <47245303+ConnollySekai@users.noreply.github.com>
2019-11-20 21:25:03 -0500

    Update head.blade.php

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,14 +1,15 @@
 <head>
 
  <!-- Global site tag (gtag.js) - Google Analytics -->
- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-152610947-1"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-153071902-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-      gtag('config', 'UA-152610947-1');
-    </script>
+  gtag('config', 'UA-153071902-1');
+</script>
+
     
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
